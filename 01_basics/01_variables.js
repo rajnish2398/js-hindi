c4ddf08118637dc885550d3b7364ac8ca0a4cdfd
rajnish2398 <147127476+rajnish2398@users.noreply.github.com>
2024-03-07 07:37:25 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 163632
+let accountEmail = "rajnishk1523@gmail.com"
+var accountPassword = "12345678"
+accountCity = "Bhilai"
+
+accountEmail = "rajnishk2398@gmail.com"
+accountPassword = "87654321"
+accountCity = "Durg"
+
+// accountId = 2 changes not allowed for const
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
