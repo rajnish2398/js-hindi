96b97ccf32c7715641fc04c58a5f9f20431be9bf
rajnish2398 <147127476+rajnish2398@users.noreply.github.com>
2024-03-07 08:43:35 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+// alert( 3 + 3) we are using nodejs, not browsers
+
+console.log(3 + 3)
+console.log("Rajnish kumar")
+
+// code should be readable
+
+let name = "Rajnish"
+let age = 26
+let isLoggedIn = false
+
+
+// number = any interger/no.
+// bigint
+// string = ""
+// boolean = true/false
+// null
+// undefined
+// symbol = unique
+
+
+// object
+
+console.log(typeof "rajnish")
