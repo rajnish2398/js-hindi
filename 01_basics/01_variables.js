b589acf4e733404487c10edee33b2d060a9890a1
rajnish2398 <147127476+rajnish2398@users.noreply.github.com>
2024-03-07 07:57:17 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,10 @@ const accountId = 163632
 let accountEmail = "rajnishk1523@gmail.com"
 var accountPassword = "12345678"
 accountCity = "Bhilai"
+let accountState; 
+
+// accountState mein undefined show hoga qki isme bs hmne declare krk chhod diya hai value ni assign kiye h
+// value assign nahi karne se undefined show hoga console mein 
 
 accountEmail = "rajnishk2398@gmail.com"
 accountPassword = "87654321"
@@ -10,4 +14,11 @@ accountCity = "Durg"
 // accountId = 2 changes not allowed for const
 
 console.log(accountId);
-console.table([accountId, accountEmail, accountPassword, accountCity])
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
