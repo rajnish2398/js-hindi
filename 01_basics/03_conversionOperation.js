012bf929c6440c55cdf3f5a90a9e8f7400fa4c1c
rajnish2398 <147127476+rajnish2398@users.noreply.github.com>
2024-03-07 09:25:46 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = false
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+
+// "100"  = 100
+// "100ncehh" = NaN
+// true = 1 ; false = 0
+
+let isLoggedIn = "Rajnish"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 = true ; 0 = false 
+// "" = false
+// "Rajnish" = true
+
+let someNumber = 100
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
