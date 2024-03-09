a127dc5e8818153b7b7346009bbfc06101c7fc4b
rajnish2398 <147127476+rajnish2398@users.noreply.github.com>
2024-03-09 09:31:32 +0000

    comparisions

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(1 > 3);
+// console.log(1 < 2);
+// console.log(1 == 2);
+// console.log(2 != 1);
+
+// console.log("2" > 1); 
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null != 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined != 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+// === triple equal to mein convert nahi karta hai datatypes bhi check krta hai
+console.log("2" == 2);
+
+console.log("2" === 2);
