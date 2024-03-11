20dd313af7eb31e73077c3899aeb08df4d72a8ca
rajnish2398 <147127476+rajnish2398@users.noreply.github.com>
2024-03-11 06:36:14 +0000

    container config done

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,7 @@
+// primitive datatypes 
+// call by value 
+// 7 types : String , Number , Boolean , Null , Undefined , Symbol , Bigint
+
+// non primitive datatypes or reference types
+
+// Array , Objects , Functions
