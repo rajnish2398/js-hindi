5e8226c3f2514995406983024ad41216e0765428
rajnish2398 <147127476+rajnish2398@users.noreply.github.com>
2024-03-13 12:11:09 +0000

    done

---
+++ 01_basics/06_PracticeSet_01-05.js
@@ -0,0 +1,12 @@
+// chapter 1 Q.no 1
+let a = "Rajnish"
+let b = 12
+let c = (a + b);
+console.log(c);
+
+// question 2
+console.log(typeof c);
+console.log(typeof b);
+console.log(typeof (a + b));
+
+// Question 3
